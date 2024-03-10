45f1f6b4fc7ed5e9283977faea8028ef8a3aa8f2
Jim Carr <jfcarr@gmail.com>
2024-03-10 10:39:09 -0400

    Convert -> Local Civil Time <-> Universal Time

--- lib/PAMacros.php
+++ lib/PAMacros.php
@@ -62,3 +62,101 @@ function decimalHoursSecond($decimalHours)
 
     return $d;
 }
+
+/**
+ * Convert a Greenwich Date/Civil Date (day,month,year) to Julian Date
+ *
+ * Original macro name: CDJD
+ */
+function civilDateToJulianDate($day, $month, $year)
+{
+    $fDay = (float) $day;
+    $fMonth = (float) $month;
+    $fYear = (float) $year;
+
+    $y = ($fMonth < 3) ? $fYear - 1 : $fYear;
+    $m = ($fMonth < 3) ? $fMonth + 12 : $fMonth;
+
+    $b = 0; // Initialize $b
+
+    if ($fYear > 1582) {
+        $a = floor($y / 100);
+        $b = 2 - $a + floor($a / 4);
+    } else {
+        if ($fYear == 1582 && $fMonth > 10) {
+            $a = floor($y / 100);
+            $b = 2 - $a + floor($a / 4);
+        } else {
+            if ($fYear == 1582 && $fMonth == 10 && $day >= 15) {
+                $a = floor($y / 100);
+                $b = 2 - $a + floor($a / 4);
+            }
+        }
+    }
+
+    $c = ($y < 0) ? floor((365.25 * $y) - 0.75) : floor(365.25 * $y);
+    $d = floor(30.6001 * ($m + 1.0));
+
+    return $b + $c + $d + $fDay + 1720994.5;
+}
+
+
+/**
+ * Returns the day part of a Julian Date
+ *
+ * Original macro name: JDCDay
+ */
+function julianDateDay($julianDate)
+{
+    $i = floor($julianDate + 0.5);
+    $f = $julianDate + 0.5 - $i;
+    $a = floor(($i - 1867216.25) / 36524.25);
+    $b = ($i > 2299160) ? $i + 1 + $a - floor($a / 4) : $i;
+    $c = $b + 1524;
+    $d = floor(($c - 122.1) / 365.25);
+    $e = floor(365.25 * $d);
+    $g = floor(($c - $e) / 30.6001);
+
+    return $c - $e + $f - floor(30.6001 * $g);
+}
+
+/**
+ * Returns the month part of a Julian Date
+ *
+ * Original macro name: JDCMonth
+ */
+function julianDateMonth($julianDate)
+{
+    $i = floor($julianDate + 0.5);
+    $a = floor(($i - 1867216.25) / 36524.25);
+    $b = ($i > 2299160) ? $i + 1 + $a - floor($a / 4) : $i;
+    $c = $b + 1524;
+    $d = floor(($c - 122.1) / 365.25);
+    $e = floor(365.25 * $d);
+    $g = floor(($c - $e) / 30.6001);
+
+    $returnValue = ($g < 13.5) ? $g - 1 : $g - 13;
+
+    return  $returnValue;
+}
+
+/**
+ * Returns the year part of a Julian Date
+ *
+ * Original macro name: JDCYear
+ */
+function julianDateYear($julianDate)
+{
+    $i = floor($julianDate + 0.5);
+    $a = floor(($i - 1867216.25) / 36524.25);
+    $b = ($i > 2299160) ? $i + 1.0 + $a - floor($a / 4.0) : $i;
+    $c = $b + 1524;
+    $d = floor(($c - 122.1) / 365.25);
+    $e = floor(365.25 * $d);
+    $g = floor(($c - $e) / 30.6001);
+    $h = ($g < 13.5) ? $g - 1 : $g - 13;
+
+    $returnValue = ($h > 2.5) ? $d - 4716 : $d - 4715;
+
+    return  $returnValue;
+}
