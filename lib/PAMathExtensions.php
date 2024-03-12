3b78554e9834c126c4e857a3fa9e6c0a45036036
Jim Carr <jfcarr@gmail.com>
2024-03-11 20:33:02 -0400

    Use built-in math functions

--- lib/PAMathExtensions.php
+++ lib/PAMathExtensions.php
@@ -3,65 +3,17 @@
 namespace PA\MathExtensions;
 
 /**
- * Returns the angle whose cosine is the specified number.
- */
-function a_cosine($d)
-{
-    return acos($d);
-}
-
-/**
- * Returns the angle whose tangent is the specified number.
- */
-function angle_tangent($d)
-{
-    return atan($d);
-}
-
-/**
- * Returns the angle whose tangent is the quotient of two specified numbers.
- */
-function angle_tangent2($y, $x)
-{
-    return atan2($y, $x);
-}
-
-/**
- * Returns the asine
- */
-function a_sine($d)
-{
-    return asin($d);
-}
-
-/**
- * Returns the cosine
- */
-function cosine($d)
-{
-    return cos($d);
-}
-
-/**
- * Calculate base 10 logarithm
- */
-function log10($d)
-{
-    return log10($d);
-}
-
-/**
- * Returns the sine
- */
-function sine($d)
-{
-    return sin($d);
-}
-
-/**
  * Convert degrees to radians
  */
-function to_radians($degrees)
+function degrees_to_radians($degrees)
 {
     return $degrees * (pi() / 180);
+}
+
+/**
+ * Convert radians to degrees
+ */
+function radians_to_degrees($radians)
+{
+    return $radians * pi() / 180;
 }
