d9b8daff5710871c2bc44246128a75620516f6d7
Jim Carr <jim.carr@devtoprd.com>
2024-10-20 13:35:29 -0400

    Refactor DateTime assertions

--- testDateTime.php
+++ testDateTime.php
@@ -6,117 +6,139 @@ include_once 'lib/PADateTime.php';
 
 use PA\DateTime as PA_DateTime;
 
+use function PA\Utils\descriptive_assert;
+
 function date_of_easter($inputYear, $expectedMonth, $expectedDay)
 {
+    $title = "Date of Easter";
+
     list($month, $day, $year) = PA_DateTime\get_date_of_easter($inputYear);
 
-    assert($month == $expectedMonth);
-    assert($day = $expectedDay);
-    assert($year = $inputYear);
+    descriptive_assert("[{$title}] Month", $month, $expectedMonth);
+    descriptive_assert("[{$title}] Day", $day, $expectedDay);
+    descriptive_assert("[{$title}] Year", $year, $inputYear);
 
-    echo "Date of Easter for {$inputYear} is {$month}/{$day}/{$year}\n";
+    echo "[{$title}] PASSED\n";
 }
 
 function civil_date_to_day_number($month, $day, $year, $expectedDayNumber)
 {
+    $title = "Civil Date to Day Number";
+
     $dayNumber = PA_DateTime\civil_date_to_day_number($month, $day, $year);
 
-    assert($dayNumber == $expectedDayNumber);
+    descriptive_assert("[{$title}] Day Number", $dayNumber, $expectedDayNumber);
 
-    echo "{$month}/{$day}/{$year} is day {$dayNumber} of the year\n";
+    echo "[{$title}] ({$expectedDayNumber}) PASSED\n";
 }
 
 function civil_time_to_decimal_hours($hours, $minutes, $seconds, $expectedDecimalHours)
 {
+    $title = "Civil Time to Decimal Hours";
+
     $decimalHours = round(PA_DateTime\civil_time_to_decimal_hours($hours, $minutes, $seconds), 8);
 
-    assert($decimalHours == $expectedDecimalHours);
+    descriptive_assert("[{$title}] Decimal Hours", $decimalHours, $expectedDecimalHours);
 
-    echo "Decimal Hours for {$hours}:{$minutes}:{$seconds} are {$decimalHours}\n";
+    echo "[{$title}] PASSED\n";
 }
 
 function decimal_hours_to_civil_time($decimalHours, $expectedHours, $expectedMinutes, $expectedSeconds)
 {
+    $title = "Decimal Hours to Civil Time";
+
     list($hours, $minutes, $seconds) = PA_DateTime\decimal_hours_to_civil_time($decimalHours);
 
-    assert($hours == $expectedHours);
-    assert($minutes == $expectedMinutes);
-    assert($seconds == $expectedSeconds);
+    descriptive_assert("[{$title}] Hours", $hours, $expectedHours);
+    descriptive_assert("[{$title}] Minutes", $minutes, $expectedMinutes);
+    descriptive_assert("[{$title}] Minutes", $seconds, $expectedSeconds);
 
-    echo "Hours, minutes, and seconds for {$decimalHours} are {$hours}:{$minutes}:{$seconds}\n";
+    echo "[{$title}] PASSED\n";
 }
 
 function local_civil_time_to_universal_time($lctHours, $lctMinutes, $lctSeconds, $isDaylightSavings, $zoneCorrection, $localDay, $localMonth, $localYear, $expectedHours, $expectedMinutes, $expectedSeconds, $expectedDay, $expectedMonth, $expectedYear)
 {
+    $title = "Local Civil Time to Universal Time";
+
     list($hours, $minutes, $seconds, $day, $month, $year) = PA_DateTime\local_civil_time_to_universal_time($lctHours, $lctMinutes, $lctSeconds, $isDaylightSavings, $zoneCorrection, $localDay, $localMonth, $localYear);
 
-    assert($hours == $expectedHours);
-    assert($minutes == $expectedMinutes);
-    assert($seconds == $expectedSeconds);
-    assert($day == $expectedDay);
-    assert($month == $expectedMonth);
-    assert($year == $expectedYear);
+    descriptive_assert("[{$title}] Hours", $hours, $expectedHours);
+    descriptive_assert("[{$title}] Minutes", $minutes, $expectedMinutes);
+    descriptive_assert("[{$title}] Seconds", $seconds, $expectedSeconds);
+    descriptive_assert("[{$title}] Day", $day, $expectedDay);
+    descriptive_assert("[{$title}] Month", $month, $expectedMonth);
+    descriptive_assert("[{$title}] Year", $year, $expectedYear);
 
-    echo "[Local] {$lctHours}:{$lctMinutes}:{$lctSeconds} {$localMonth}/{$localDay}/{$localYear} = [Universal] {$hours}:{$minutes}:{$seconds} {$month}/{$day}/{$year}\n";
+    echo "[{$title}] PASSED\n";
 }
 
 function universal_time_to_local_civil_time($utHours, $utMinutes, $utSeconds, $isDaylightSavings, $zoneCorrection, $gwDay, $gwMonth, $gwYear, $expectedHours, $expectedMinutes, $expectedSeconds, $expectedDay, $expectedMonth, $expectedYear)
 {
+    $title = "Universal Time to Local Civil Time";
+
     list($hours, $minutes, $seconds, $day, $month, $year) = PA_DateTime\universal_time_to_local_civil_time_dt($utHours, $utMinutes, $utSeconds, $isDaylightSavings, $zoneCorrection, $gwDay, $gwMonth, $gwYear);
 
-    assert($hours == $expectedHours);
-    assert($minutes == $expectedMinutes);
-    assert($seconds == $expectedSeconds);
-    assert($day == $expectedDay);
-    assert($month == $expectedMonth);
-    assert($year == $expectedYear);
+    descriptive_assert("[{$title}] Hours", $hours, $expectedHours);
+    descriptive_assert("[{$title}] Minutes", $minutes, $expectedMinutes);
+    descriptive_assert("[{$title}] Seconds", $seconds, $expectedSeconds);
+    descriptive_assert("[{$title}] Day", $day, $expectedDay);
+    descriptive_assert("[{$title}] Month", $month, $expectedMonth);
+    descriptive_assert("[{$title}] Year", $year, $expectedYear);
 
-    echo "[Universal] {$utHours}:{$utMinutes}:{$utSeconds} {$gwMonth}/{$gwDay}/{$gwYear} = [Local] {$hours}:{$minutes}:{$seconds} {$month}/{$day}/{$year}\n";
+    echo "[{$title}] PASSED\n";
 }
 
 function universal_time_to_greenwich_sidereal_time($utHours, $utMinutes, $utSeconds, $gwDay, $gwMonth, $gwYear, $expectedHours, $expectedMinutes, $expectedSeconds)
 {
+    $title = "Universal Time to Greenwich Sidereal Time";
+
     list($hours, $minutes, $seconds) = PA_DateTime\universal_time_to_greenwich_sidereal_time($utHours, $utMinutes, $utSeconds, $gwDay, $gwMonth, $gwYear);
 
-    assert($hours == $expectedHours);
-    assert($minutes == $expectedMinutes);
-    assert($seconds == $expectedSeconds);
+    descriptive_assert("[{$title}] Hours", $hours, $expectedHours);
+    descriptive_assert("[{$title}] Minutes", $minutes, $expectedMinutes);
+    descriptive_assert("[{$title}] Seconds", $seconds, $expectedSeconds);
 
-    echo "[Universal] {$gwMonth}/{$gwDay}/{$gwYear} {$utHours}:{$utMinutes}:{$utSeconds} = [Greenwich] {$hours}:{$minutes}:{$seconds}\n";
+    echo "[{$title}] PASSED\n";
 }
 
 function greenwich_sidereal_time_to_universal_time($gstHours, $gstMinutes, $gstSeconds, $gwDay, $gwMonth, $gwYear, $expectedHours, $expectedMinutes, $expectedSeconds, $expectedWarningFlag)
 {
+    $title = "Greenwich Sidereal Time to Universal Time";
+
     list($hours, $minutes, $seconds, $warningFlag) = PA_DateTime\greenwich_sidereal_time_to_universal_time($gstHours, $gstMinutes, $gstSeconds, $gwDay, $gwMonth, $gwYear);
 
-    assert($hours == $expectedHours);
-    assert($minutes == $expectedMinutes);
-    assert($seconds == $expectedSeconds);
-    assert($warningFlag == $expectedWarningFlag);
+    descriptive_assert("[{$title}] Hours", $hours, $expectedHours);
+    descriptive_assert("[{$title}] Minutes", $minutes, $expectedMinutes);
+    descriptive_assert("[{$title}] Seconds", $seconds, $expectedSeconds);
+    descriptive_assert("[{$title}] Warning Flag", $warningFlag, $expectedWarningFlag);
 
-    echo "[Greenwich] {$gwMonth}/{$gwDay}/{$gwYear} {$gstHours}:{$gstMinutes}:{$gstSeconds} = [Universal] {$hours}:{$minutes}:{$seconds}\n";
+    echo "[{$title}] PASSED\n";
 }
 
 function greenwich_sidereal_time_to_local_sidereal_time($gstHours, $gstMinutes, $gstSeconds, $geographicalLongitude, $expectedHours, $expectedMinutes, $expectedSeconds)
 {
+    $title = "Greenwich Sidereal Time to Local Sidereal Time";
+
     list($hours, $minutes, $seconds)  = PA_DateTime\greenwich_sidereal_time_to_local_sidereal_time($gstHours, $gstMinutes, $gstSeconds, $geographicalLongitude);
 
-    assert($hours == $expectedHours);
-    assert($minutes == $expectedMinutes);
-    assert($seconds == $expectedSeconds);
+    descriptive_assert("[{$title} Hours", $hours, $expectedHours);
+    descriptive_assert("[{$title} Minutes", $minutes, $expectedMinutes);
+    descriptive_assert("[{$title} Seconds", $seconds, $expectedSeconds);
 
-    echo "[Greenwich] {$gstHours}:{$gstMinutes}:{$gstSeconds} = [Sidereal] {$hours}:{$minutes}:{$seconds}\n";
+    echo "[{$title}] PASSED\n";
 }
 
 function local_sidereal_time_to_greenwich_sidereal_time($lstHours, $lstMinutes, $lstSeconds, $geographicalLongitude, $expectedHours, $expectedMinutes, $expectedSeconds)
 {
+    $title = "Local Sidereal Time to Greenwich Sidereal Time";
+
     list($hours, $minutes, $seconds) = PA_DateTime\local_sidereal_time_to_greenwich_sidereal_time($lstHours, $lstMinutes, $lstSeconds, $geographicalLongitude);
 
-    assert($hours == $expectedHours);
-    assert($minutes == $expectedMinutes);
-    assert($seconds == $expectedSeconds);
+    descriptive_assert("[{$title} Hours", $hours, $expectedHours);
+    descriptive_assert("[{$title} Minutes", $minutes, $expectedMinutes);
+    descriptive_assert("[{$title} Seconds", $seconds, $expectedSeconds);
 
-    echo "[Sidereal] {$lstHours}:{$lstMinutes}:{$lstSeconds} = [Greenwich] {$hours}:{$minutes}:{$seconds}\n";
+    echo "[{$title}] PASSED\n";
 }
 
 date_of_easter(2023, 4, 9);
